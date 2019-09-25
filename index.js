7ad97056e1ad9c707f5dd1e40f3de06c4910aec5
Robert Jackson <rjackson@linkedin.com>
2019-09-25 19:48:41 -0400

    Initial / simple implementation.

--- index.js
+++ index.js
@@ -2,5 +2,88 @@
 
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
+
+function updatePackageJSON(root) {
+  let packageJSONPath = path.join(root, 'package.json');
+  let pkg = JSON.parse(fs.readFileSync(packageJSONPath, { encoding: 'utf-8' }));
+
+  pkg.devDependencies = pkg.devDependencies || {};
+  pkg.devDependencies['@ember/edition-utils'] = '^1.1.1';
+  pkg.devDependencies['@ember/optional-features'] = '^1.0.0';
+
+  fs.writeFileSync(packageJSONPath, JSON.stringify(pkg, null, 2), { encoding: 'utf-8' });
+}
+
+function migrateEmberCLIFile(root) {
+  let newConfigPath = path.join(root, '.ember-cli.js');
+  let oldConfigPath = path.join(root, '.ember-cli');
+
+  if (fs.existsSync(newConfigPath)) {
+    return false;
+  }
+
+  if (!fs.existsSync(oldConfigPath)) {
+    throw new Error(
+      'Does not seem to be a valid ember-cli project, please run `npx @ember/octanify` from within your ember-cli application or addon directly'
+    );
+  }
+
+  let existingContents = fs.readFileSync(oldConfigPath, { encoding: 'utf-8' });
+
+  let updatedContents = `'use strict';
+const { setEdition } = require('@ember/edition-utils');
+
+setEdition('octane');
+
+module.exports = ${existingContents.trimRight()};\n`;
+
+  fs.writeFileSync(newConfigPath, updatedContents, { encoding: 'utf-8' });
+
+  fs.unlinkSync(oldConfigPath);
+}
+
+async function updateOptionalFeatures(root) {
+  let packageJSONPath = path.join(root, 'package.json');
+  let pkg = JSON.parse(fs.readFileSync(packageJSONPath, { encoding: 'utf-8' }));
+
+  let configPath = 'config/optional-features.json';
+  if (pkg['ember-addon'] && pkg['ember-addon'].configPath) {
+    configPath = pkg['ember-addon'].configPath;
+  }
+
+  let config = {};
+  if (fs.existsSync(configPath)) {
+    let configContents = fs.readFileSync(configPath, { encoding: 'utf-8' });
+    config = JSON.parse(configContents);
+  }
+
+  config['application-template-wrapper'] = false;
+  config['default-async-observers'] = true;
+  config['jquery-integration'] = false;
+  config['template-only-glimmer-components'] = true;
+
+  let keys = Object.keys(config);
+  let newConfig = {};
+  keys.sort().forEach(key => (newConfig[key] = config[key]));
+
+  fs.writeFileSync(configPath, JSON.stringify(newConfig, null, 2), { encoding: 'utf-8' });
+}
+
+async function main() {
+  let root = process.cwd();
+
+  try {
+    await migrateEmberCLIFile(root);
+    await updatePackageJSON(root);
+    await updateOptionalFeatures(root);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
 if (require.main === module) {
+  main();
 }
--- test.js
+++ test.js
@@ -1,5 +1,103 @@
-const { test } = QUnit;
+const { createTempDir } = require('broccoli-test-helper');
+const execa = require('execa');
 
-QUnit.module('@ember/octanify', function() {
-  test('it works on a basic ember-cli@3.13 app structure', function() {});
+const { test } = QUnit;
+const ROOT = process.cwd();
+const OCTANIFY_PATH = require.resolve('./index');
+
+QUnit.module('@ember/octanify', function(hooks) {
+  let project;
+
+  hooks.beforeEach(async function() {
+    project = await createTempDir();
+    process.chdir(project.path());
+  });
+
+  hooks.afterEach(async function() {
+    process.chdir(ROOT);
+    await project.dispose();
+  });
+
+  test('it works on a basic ember-cli@3.13 app structure', async function(assert) {
+    project.write({
+      '.ember-cli': `{
+  /**
+    Ember CLI sends analytics information by default. The data is completely
+    anonymous, but there are times when you might want to disable this behavior.
+
+    Setting \`disableAnalytics\` to true will prevent any data from being sent.
+  */
+  "disableAnalytics": false
+}
+`,
+      config: {
+        'optional-features.json': `{\n  "jquery-integration": false\n}`,
+      },
+      'package.json': JSON.stringify(
+        {
+          name: 'foo',
+          devDependencies: {
+            '@ember/optional-features': '^0.7.0',
+            'ember-cli': '*',
+          },
+        },
+        null,
+        2
+      ),
+    });
+
+    await execa(OCTANIFY_PATH);
+
+    let expected = {
+      '.ember-cli.js': `'use strict';
+const { setEdition } = require('@ember/edition-utils');
+
+setEdition('octane');
+
+module.exports = {
+  /**
+    Ember CLI sends analytics information by default. The data is completely
+    anonymous, but there are times when you might want to disable this behavior.
+
+    Setting \`disableAnalytics\` to true will prevent any data from being sent.
+  */
+  "disableAnalytics": false
+};
+`,
+      config: {
+        'optional-features.json': JSON.stringify(
+          {
+            'application-template-wrapper': false,
+            'default-async-observers': true,
+            'jquery-integration': false,
+            'template-only-glimmer-components': true,
+          },
+          null,
+          2
+        ),
+      },
+      'package.json': JSON.stringify(
+        {
+          name: 'foo',
+          devDependencies: {
+            '@ember/optional-features': '^1.0.0',
+            'ember-cli': '*',
+            '@ember/edition-utils': '^1.1.1',
+          },
+        },
+        null,
+        2
+      ),
+    };
+
+    assert.deepEqual(project.read(), expected, 'running against standard 3.13 project works');
+
+    await execa(OCTANIFY_PATH);
+
+    assert.deepEqual(
+      project.read(),
+      expected,
+      'running on already converted project has no effect'
+    );
+  });
 });
