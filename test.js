e5806a3b0b76f32cd2461a97f813d7034b5b930c
Robert Jackson <rjackson@linkedin.com>
2019-12-19 22:40:43 -0500

    Refactor test to spawn `@ember/optional-features`.

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const fs = require('fs-extra');
+const path = require('path');
 const { createTempDir } = require('broccoli-test-helper');
 const execa = require('execa');
 
@@ -5,12 +7,43 @@ const { test } = QUnit;
 const ROOT = process.cwd();
 const OCTANIFY_PATH = require.resolve('./index');
 
+async function octanify() {
+  return execa(OCTANIFY_PATH, ['--run-codemod']);
+}
+
 QUnit.module('@ember/octanify', function(hooks) {
   let project;
 
-  hooks.beforeEach(async function() {
+  hooks.beforeEach(async function(assert) {
     project = await createTempDir();
+
+    project.write({
+      'package.json': JSON.stringify({
+        name: 'dummy',
+        description: '',
+        version: '0.0.0',
+        devDependencies: {
+          '@ember/optional-features': '*',
+          'ember-cli': '*',
+          'ember-source': '*',
+        },
+      }),
+    });
+
     process.chdir(project.path());
+
+    fs.symlinkSync(path.join(ROOT, 'node_modules'), path.join(project.path(), 'node_modules'));
+
+    assert.compareProject = function(expected, message) {
+      let actual = {
+        'package.json': project.readText('package.json'),
+        config: {
+          'optional-features.json': project.readText('config/optional-features.json'),
+        },
+      };
+
+      this.deepEqual(actual, expected, message);
+    };
   });
 
   hooks.afterEach(async function() {
@@ -20,16 +53,6 @@ QUnit.module('@ember/octanify', function(hooks) {
 
   test('it works on a basic ember-cli@3.13 app structure', async function(assert) {
     project.write({
-      '.ember-cli': `{
-  /**
-    Ember CLI sends analytics information by default. The data is completely
-    anonymous, but there are times when you might want to disable this behavior.
-
-    Setting \`disableAnalytics\` to true will prevent any data from being sent.
-  */
-  "disableAnalytics": false
-}
-`,
       config: {
         'optional-features.json': `{\n  "jquery-integration": false\n}`,
       },
@@ -46,49 +69,32 @@ QUnit.module('@ember/octanify', function(hooks) {
       ),
     });
 
-    let childProcessResult = await execa(OCTANIFY_PATH);
-    console.log('project', project);
-    console.log('childProcessResult', childProcessResult);
-
-    assert.equal(childProcessResult.exitCode, 0, 'child process exited with code of 0');
-    assert.equal(childProcessResult.stderr, '', 'Nothing is logged to stderr');
+    await octanify();
 
     let expected = {
-      '.ember-cli.js': `'use strict';
-const { setEdition } = require('@ember/edition-utils');
-
-setEdition('octane');
-
-module.exports = {
-  /**
-    Ember CLI sends analytics information by default. The data is completely
-    anonymous, but there are times when you might want to disable this behavior.
-
-    Setting \`disableAnalytics\` to true will prevent any data from being sent.
-  */
-  "disableAnalytics": false
-};
-`,
       config: {
-        'optional-features.json': JSON.stringify(
-          {
-            'application-template-wrapper': false,
-            'default-async-observers': true,
-            'jquery-integration': false,
-            'template-only-glimmer-components': true,
-          },
-          null,
-          2
-        ),
+        'optional-features.json':
+          JSON.stringify(
+            {
+              'application-template-wrapper': false,
+              'default-async-observers': true,
+              'jquery-integration': false,
+              'template-only-glimmer-components': true,
+            },
+            null,
+            2
+          ) + '\n',
       },
       'package.json': JSON.stringify(
         {
           name: 'foo',
           devDependencies: {
-            '@ember/optional-features': '^1.0.0',
+            '@ember/optional-features': '^1.3.0',
             'ember-cli': '*',
-            '@ember/edition-utils': '^1.1.1',
-            '@glimmer/component': '^0.14.0-alpha.13',
+            '@glimmer/component': '^1.0.0',
+          },
+          ember: {
+            edition: 'octane',
           },
         },
         null,
@@ -96,14 +102,10 @@ module.exports = {
       ),
     };
 
-    assert.deepEqual(project.read(), expected, 'running against standard 3.13 project works');
+    assert.compareProject(expected, 'running against standard 3.13 project works');
 
-    await execa(OCTANIFY_PATH);
+    await octanify();
 
-    assert.deepEqual(
-      project.read(),
-      expected,
-      'running on already converted project has no effect'
-    );
+    assert.compareProject(expected, 'running on already converted project has no effect');
   });
 });
